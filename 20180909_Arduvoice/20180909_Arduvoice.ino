94ff43955097de8801aceb71df15f0bd918b4a9c
Wolinam <zmffhqj35@naver.com>
2020-02-04 23:09:11 +0900

    Add files via upload

---
+++ 20180909_Arduvoice/20180909_Arduvoice.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+
+void setup() {
+  // put your setup code here, to run once:
+  servo1.attach(3);
+  servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(9);
+  servo5.attach(10);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int i=0;i<180;i++){
+  servo1.write(i);
+  servo2.write(i);
+  servo3.write(i);
+  servo4.write(i);
+  servo5.write(i);
+  delay(10);
+  }
+  for(int i=180;i>0;i--){
+    servo1.write(i);
+    servo2.write(i);
+    servo3.write(i);
+    servo4.write(i);
+    servo5.write(i);
+   delay(10);
+    }
+}
